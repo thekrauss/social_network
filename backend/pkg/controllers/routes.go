3ef9ac8f17fcc7702b8cf4581e97e1bf9972b61f
vkraussl <thekrauss31@gmail.com>
2024-10-08 21:43:53 +0200

    add routes

--- backend/pkg/controllers/routes.go
+++ backend/pkg/controllers/routes.go
@@ -1,6 +1,11 @@
 package controllers
 
 func (s *MyServer) routes() {
-	//s.Router.Handle("/frontEnd/", http.StripPrefix("/frontEnd/", http.FileServer(http.Dir("frontEnd"))))
-	s.Router.Handle("/register", Chain(s.RegisterHandler(), LogRequestMiddleware))
+	//	s.Router.Handle("/register", Chain(s.RegisterHandler(), LogRequestMiddleware))
+	s.Router.Handle("/login", Chain(s.LoginHandler(), LogRequestMiddleware))
+	s.Router.Handle("/create_post", Chain(s.PostHandlers(), LogRequestMiddleware))
+	s.Router.Handle("/list_post", Chain(s.ListPostHandler(), LogRequestMiddleware))
+	s.Router.Handle("/create_comment", Chain(s.CreateCommentHandler(), LogRequestMiddleware))
+	s.Router.Handle("/list_comment", Chain(s.ListCommentHandler(), LogRequestMiddleware))
+
 }
